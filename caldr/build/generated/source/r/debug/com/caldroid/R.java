88e9e18b26c4ec3db5fd223c5cd31d584ef1af0d
Aliaksandr <Kureichyk>
2017-02-28 18:56:32 +0300

    изменил активити

--- caldr/build/generated/source/r/debug/com/caldroid/R.java
+++ caldr/build/generated/source/r/debug/com/caldroid/R.java
@@ -30,14 +30,14 @@ public final class R {
 		public static final int right_arrow = 0x7f020008;
 	}
 	public static final class id {
-		public static final int calendar_gridview = 0x7f0a0016;
-		public static final int calendar_left_arrow = 0x7f0a0010;
-		public static final int calendar_month_year_textview = 0x7f0a0011;
-		public static final int calendar_right_arrow = 0x7f0a0012;
-		public static final int calendar_title_view = 0x7f0a000f;
-		public static final int calendar_tv = 0x7f0a0015;
-		public static final int months_infinite_pager = 0x7f0a0014;
-		public static final int weekday_gridview = 0x7f0a0013;
+		public static final int calendar_gridview = 0x7f0a0015;
+		public static final int calendar_left_arrow = 0x7f0a000f;
+		public static final int calendar_month_year_textview = 0x7f0a0010;
+		public static final int calendar_right_arrow = 0x7f0a0011;
+		public static final int calendar_title_view = 0x7f0a000e;
+		public static final int calendar_tv = 0x7f0a0014;
+		public static final int months_infinite_pager = 0x7f0a0013;
+		public static final int weekday_gridview = 0x7f0a0012;
 	}
 	public static final class layout {
 		public static final int calendar_view = 0x7f040002;
